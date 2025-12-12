0ab49f9aa0f75ca6f208d3d5da866488c2fd36be
Guilhermemagalhaess''git config --global user.email <gui.metal21@gmail.com>
2025-12-12 14:18:59 -0300

    Concatenação automaticamente com o valor

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 alert("Primeiro projeto em JS");
-let numeroSecreto = 29;
+let numeroSecreto = 15;
 console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 30');
 
 // se o chute for igual ao número secreto
 if (numeroSecreto == chute){
-    alert('Isso aí! VocÊ descobriu o número secreto (5)');
+alert(`Isso aí! VocÊ descobriu o número secreto ${numeroSecreto}`);
 } else {
 alert ("Você errou :")
 }
