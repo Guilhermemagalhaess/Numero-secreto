6478db34315b7c5d3da9e9c5e7b3d8a033483ea3
Guilhermemagalhaess''git config --global user.email <gui.metal21@gmail.com>
2025-12-12 14:06:16 -0300

    Usando e entendendo o else

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 alert("Primeiro projeto em JS");
 let numeroSecreto = 29;
+console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 30');
 
+// se o chute for igual ao número secreto
 if (numeroSecreto == chute){
-    console.log('Isso aí! VocÊ descobriu o número secreto (5)');
+    alert('Isso aí! VocÊ descobriu o número secreto (5)');
+} else {
+alert ("Você errou :")
 }
